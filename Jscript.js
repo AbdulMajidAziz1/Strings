3e6f93c38cc415e3dc5715e69887af9053fbb5fa
Abdul Majid <abdulmajidaziz0304@gmail.com>
2023-03-09 15:17:17 -0800

    Strings

---
+++ Jscript.js
@@ -0,0 +1,11 @@
+let Name = "Majid Aziz"
+let greetings = "Hi, my name is "
+
+let mygreetings = greetings + " " + Name 
+
+console.log(mygreetings)
+
+let message = document.getElementById("welcome")
+
+
+message.innerText = mygreetings
